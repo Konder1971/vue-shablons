64bcece1586e80ee3439266040cbaf34e1b171ae
Konder <konder1971@mail.ru>
2020-02-03 22:52:35 +0200

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ now }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      now: new Date()
+    }
+  },
+  methods: {
+    updateDate() {
+      this.now = new Date();
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.updateDate();
+    }, 1000);
+  }
+
+}
+</script>
+
+<style scoped>
+p{
+  margin: 20px 0;
+}
+</style>
