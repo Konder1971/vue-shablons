58410c8654fdfebc9bf8603d7b78c5bcdc8bfe4f
Konder1971 <konder1971@mail.ru>
2020-02-04 16:27:02 +0200

    vr

---
+++ JsСollection.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="jscollection-container">
+    <p><a target="_blank" href="https://www.youtube.com/playlist?list=PLQtgBC2b8xGPU9MG8STbsQo0-H3v2wims&fbclid=IwAR2lSMdqV7zeW14DDIDaD8mT93mrJMzeEjcxFYaxjkSSfuGAt-bCrXiawBU&app=desktop">Коллекция художественных учебных фильмов и мультфильмов</a></p>
+    <p><a href="https://www.youtube.com/watch?v=0-aNTyFKMEI">https://www.youtube.com/watch?v=0-aNTyFKMEI</a></p>
+    <h1>JavaScript Collection</h1>
+    <h2>Мантры земных стихий </h2>
+    <p>Работа с мантрами предполагает поэтапное освоение энергий различного уровня. Приведенные ниже мантры являются мантрами, которые применяются в программе Школы Йога Гуру Ар Сантэма на начальном уровне в практике <a href="/mantra-yoga-mantra-om.htm">мантра-йоги</a>. Данный перечень мантр даёт более плотный спектр энергий, что позволяет занимающимся за непродолжительный промежуток времени ощутить энергию этих мантр. А также, на более продвинутом этапе освоения мантра-йоги, прочувствовать отличительные свойства данных энергий и их влияние на физическую энергетическую и психологическую составляющую человека. </p>
+    <table>
+      <tr>
+        <td width="67"> Мантра </td>
+        <td width="181"> Цвет энергии </td>
+        <td width="448"> Значение </td>
+      </tr>
+      <tr>
+        <td width="67"> ЮМ </td>
+        <td width="181">Тёмно коричневый</td>
+        <td width="448">Стихия земли. Твёрдость костной системы. <br>
+          Состояние внутренней твёрдости, стержня.</td>
+      </tr>
+      <tr>
+        <td width="67"> ДЭ </td>
+        <td width="181">Светло коричневый</td>
+        <td width="448">Стихия земли. Пополнение микроэлементами органов.    Структурирование&nbsp;тела, мягких тканей.</td>
+      </tr>
+      <tr>
+        <td width="67"> ХУМ </td>
+        <td width="181">Синий шёлк</td>
+        <td width="448">Стихия воды. Замена внутренних вод в организме.<br>
+          Состояние плавности, текучести</td>
+      </tr>
+      <tr>
+        <td width="67"> ЦЮ </td>
+        <td width="181">Светло красный, розовый</td>
+        <td width="448">Возбуждение и нормализация эндокринной системы</td>
+      </tr>
+      <tr>
+        <td width="67"> АХ </td>
+        <td width="181">Красный шёлк</td>
+        <td width="448">Стихия огня. Чистка земных энергий.<br>
+          Состояние энергичности, движения&hellip;</td>
+      </tr>
+      <tr>
+        <td width="67"> ЗЭЖ </td>
+        <td width="181">Светло синий, <br>
+          голубой</td>
+        <td width="448">Стихия воздуха. Замена внутренних ветров в организме.<br>
+          Состояние лёгкости</td>
+      </tr>
+      <tr>
+        <td width="67"> ИРИ </td>
+        <td width="181">Светло голубой,<br>
+    белёсый</td>
+        <td width="448">Стихия эфира</td>
+      </tr>
+      <tr>
+        <td width="67"> БУХ </td>
+        <td width="181">Светло зелёный</td>
+        <td width="448">Стихия дерева (рост).</td>
+      </tr>
+      <tr>
+        <td width="67"> ОМ </td>
+        <td width="181">Белый шёлк</td>
+        <td width="448">Женская  Вселенская энергия. <br>
+        Присутствует везде, где происходит рождение, зачатие.</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'jscollection',
+  components: {
+    //jcscontent, jcssidebar
+  }
+}
+</script>
+
+<style scoped>
+table {
+  margin: 0 auto 20px;
+  padding: 0 20px;
+  text-align: left;
+}
+table td {
+  padding: 10px;
+  border: 1px solid #333;
+}
+h2 + p {
+  max-width: 1000px;
+  margin: 8px auto 20px;
+}
+</style>
